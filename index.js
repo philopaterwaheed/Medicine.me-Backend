b3e16a7b5eeb74a0c663040971f5da030b360992
Moe <71234244+moe-the-techie@users.noreply.github.com>
2024-12-20 21:15:55 +0200

    trying to deploy

--- index.js
+++ index.js
@@ -37,6 +37,5 @@ mongoose
   app.listen(process.env.PORT || 5000, () => {
     console.log(`Backend server is running! Connected to Port ${process.env.PORT || 5000}`);
   });
-
-  
+module.exports = app;
 
